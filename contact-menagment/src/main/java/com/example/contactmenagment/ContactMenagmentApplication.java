de30e8e76356c74c1339b85c4f0a73eded76ac3b
Andrija <astojanovic321@gmail.com>
2022-08-01 13:23:41 +0200

    first commit

---
+++ contact-menagment/src/main/java/com/example/contactmenagment/ContactMenagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactmenagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMenagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMenagmentApplication.class, args);
+	}
+
+}
---
+++ contact-menagment/src/test/java/com/example/contactmenagment/ContactMenagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactmenagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMenagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
